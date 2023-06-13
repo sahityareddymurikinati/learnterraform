4ce416ecbaf3a2e2255dcfc5198c968af1dec697
sahityareddy <sahitya.murikinati@insparx.com>
2023-06-13 18:37:54 +0200

    terraform

--- resource/main.tf
+++ resource/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "web" {
 data "aws_ami" "example" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["997210232336"]
+  owners           = ["973714476881"]
 }
 
 provider "aws" {
