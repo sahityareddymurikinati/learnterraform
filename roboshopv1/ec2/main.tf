aacd00acdbf9a288357eb1513655f7f4f9678816
sahityareddy <sahitya.murikinati@insparx.com>
2023-06-15 12:57:18 +0200

    terraform

---
+++ roboshopv1/ec2/main.tf
@@ -0,0 +1,66 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.small"
+  vpc_security_group_ids =[aws_security_group.sg.id]
+  tags = {
+    Name = var.name
+  }
+}
+
+data "aws_ami" "example" {
+  owners           = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
+
+resource "aws_security_group" "sg" {
+  name        = var.name
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sample"
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z08488763JT3V0K51XZPF"
+  name    = "${var.name}-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.web.private_ip]
+}
+variable "name" {
+
+}
+provisioner "remote-exec" {
+
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+  inline = [
+    "sudo labauto ansible",
+    "ansible-pull -i localhost, -U https://github.com/sahityareddymurikinati/roboshop-ansible.git -e env=dev -e role_name=${var.name}"
+  ]
+}
