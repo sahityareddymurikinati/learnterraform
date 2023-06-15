66e9140c2d1a1352bb362a0a73baba1f156cdf07
sahityareddy <sahitya.murikinati@insparx.com>
2023-06-15 13:22:19 +0200

    terraform

--- roboshopv1/main.tf
+++ roboshopv1/main.tf
@@ -1,49 +1,49 @@
-module "frontend"{
+module "frontend" {
   source = "./ec2"
   name = "frontend"
 }
 
-module "mongodb"{
+module "mongodb" {
   source = "./ec2"
   name = "mongodb"
 }
 
-module "catalogue"{
+module "catalogue" {
   source = "./ec2"
   name = "catalogue"
 }
 
-module "redis"{
+module "redis" {
   source = "./ec2"
   name = "redis"
 }
 
-module "user"{
+module "user" {
   source = "./ec2"
   name = "user"
 }
 
-module "cart"{
+module "cart" {
   source = "./ec2"
   name = "cart"
 }
 
-module "mysql"{
+module "mysql" {
   source = "./ec2"
   name = "mysql"
 }
 
-module "shipping"{
+module "shipping" {
   source = "./ec2"
   name = "shipping"
 }
 
-module "rabbitmq"{
+module "rabbitmq" {
   source = "./ec2"
   name = "rabbitmq"
 }
 
-module "payment"{
+module "payment" {
   source = "./ec2"
   name = "payment"
 }
